e93539ff39ec7c9ed9e50a2cff77e69d78a49777
nareshgoli <venkatanaresh515@gmail.com>
2019-09-11 23:27:23 +0530

    updated models file

--- app/models.py
+++ app/models.py
@@ -1,14 +1,9 @@
-# from __future__ import unicode_literals
 from django.db import models
 
 class Fib(models.Model):
     number = models.IntegerField()
     result = models.IntegerField()
-    time_taken = models.CharField(max_length=255)
-
-    # class Meta:
-    #     db_table = 'fibonacci_results'
-    
+    time_taken = models.CharField(max_length=255)    
 
     def __str__(self):
         return self.number
