e20b38be13fe4c806c2947532eb47cc816a70224
nareshgoli <venkatanaresh515@gmail.com>
2019-09-12 16:31:57 +0530

    updated

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Fib(models.Model):
     number = models.IntegerField()
-    result = models.FloatField()
+    result = models.CharField(max_length=100000)
     time_taken = models.CharField(max_length=255)    
 
     def __str__(self):
